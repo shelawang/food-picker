11db5eaa9aa7cd3743bd95d01dc9bd7bfad37187
Raghav Subramaniam <raghavs511@gmail.com>
2014-09-06 00:00:32 -0400

    Wrote basic interface

---
+++ logic.js
@@ -0,0 +1,15 @@
+/*
+    Given a set of search parameters, do a call to the Yelp API
+    and return the resulting Food Picker JS object.
+*/
+var fpObj = yelpAccess(limit, radius, latitude, longitude) {
+    return 0;
+};
+
+/*
+    Given a Food Picker JS object, generate a binary decision tree
+    that contains the questions being used by Food Picker.
+*/
+var questionTree = genQuestions(fpObj) {
+    return 0;
+};
