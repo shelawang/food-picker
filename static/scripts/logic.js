19ee46473ff80ad31ab1b4c87efa2486c7551013
Raghav Subramaniam <raghavs511@gmail.com>
2014-09-06 16:56:00 -0400

    I am Groot

--- static/scripts/logic.js
+++ static/scripts/logic.js
@@ -1,35 +1,112 @@
 /*
-    Given a set of search parameters, do a call to the Yelp API
-    and return the resulting Food Picker JS object.
-* @return object containing query information
+*   Given a set of search parameters, do a call to the Yelp API
+*   and return the resulting Food Picker JS object.
 */
-var fpObj = function(limit, radius, lat, long_) {
+var questionTree = function(limit, radius, lat, long_) {
     var data = {limit: limit,
                 radius: radius,
                 lat: lat,
                 'long' : long_};
-    var hooplah;
+    var ret;
 
     $.getJSON("http://localhost:5000/request", 
         data, function(result) {
             console.log('hello');
         
-            hooplah = questionTree(result);
-                console.log(hooplah);
-
+            ret = questionNode(result.businesses);
+            console.log(ret);
         });
-    return hooplah;
+    return ret;
 };
 
 /*
-    Given a Food Picker JS object, generate a binary decision tree
-    that contains the questions being used by Food Picker.
+*   Given a chunk of a Food Picker JS object, create one node of
+*   the binary decision tree.
 */
-function questionTree(fpObj) {
-    return questionNode(fpObj.businesses);
+function questionNode(candidates) {
+    var totalCount = candidates.length;
+
+    var flag;
+    var question;
+    var yes = [];
+    var no = [];
+
+    if (totalCount == 1) {
+        console.log('poop');
+        return {
+            flag: '',
+            question: '',
+            yes: null,
+            no: null,
+            candidates: candidates
+        }
+    }
+
+    var table = {};
+    for (var i = 0; i < totalCount; i++) {
+        updateTable(table, candidates[i]);
+    }
+
+    var sameCount = true
+    for (var key in table) {
+        sameCount = sameCount && (table[key] == totalCount);
+    }
+
+    if (sameCount) {
+
+        flag = 'name';
+
+        var nameRand = (Math.random() * totalCount) | 0;
+        console.log(candidates);
+        console.log(nameRand);
+        question = candidates[nameRand].name;
+
+        yes = [candidates[nameRand]];
+        for (var j = 0; j < totalCount; j++) {
+            if (j != nameRand) {
+                no.push(candidates[j]);
+            }
+        }
+
+    } else {
+    
+        flag = 'category';
+    
+        var categoryList = [];
+        var minDiff = totalCount;
+        for (var key in table) {
+            var diff = Math.abs(table[key] - totalCount / 2);
+            if (diff < minDiff) {
+                minDiff = diff;
+                categoryList = [key]
+            }
+            if (diff == minDiff) {
+                categoryList.push(key)
+            }
+        }
+    
+        var categoryRand = (Math.random() * categoryList.length) | 0;
+        question = categoryList[categoryRand];
+    
+        for (var k = 0; k < totalCount; k++) {
+            if (containsCategory(question, candidates[k])) {
+                yes.push(candidates[k]);
+            } else {
+                no.push(candidates[k]);
+            }
+        }
+    }
+
+    return {
+        question: question,
+        yes: questionNode(yes),
+        no: questionNode(no),
+        candidates: candidates
+    };
+
 };
 
-var updateTable = function(table, candidate) {
+function updateTable(table, candidate) {
     for (var i = 0; i < candidate.categories.length; i++) {
         var categoryName = candidate.categories[i][0];
         if (table.hasOwnProperty(categoryName)) {
@@ -40,7 +117,7 @@ var updateTable = function(table, candidate) {
     }
 };
 
-var containsCategory = function(questionCategory, candidate) {
+function containsCategory(questionCategory, candidate) {
     for (var i = 0; i < candidate.categories.length; i++) {
         var categoryName = candidate.categories[i][0];
         if (categoryName == questionCategory) {
@@ -51,61 +128,4 @@ var containsCategory = function(questionCategory, candidate) {
     return false;
 };
 
-/*
-    Given a chunk of a Food Picker JS object, create one node of
-    the binary decision tree.
-*/
-function questionNode(candidates) {
-    var totalCount = candidates.length;
-
-    if (totalCount == 0) {
-        return null
-    }
-
-    var table = {};
-    for (var i = 0; i < totalCount; i++) {
-        updateTable(table, candidates[i]);
-    }
-
-    var categoryList = [];
-    var minDiff = totalCount;
-    for (var key in table) {
-        var diff = Math.abs(table[key] - totalCount / 2);
-        if (diff < minDiff) {
-            minDiff = diff;
-            categoryList = [key]
-        }
-        if (diff == minDiff) {
-            categoryList.push(key)
-        }
-    }
-
-    var rand = (Math.random() * categoryList.length) | 0;
-    var category = categoryList[rand];
-
-    var yes = [];
-    var no = [];
-    for (var j = 0; j < totalCount; j++) {
-        if (containsCategory(category, candidates[j])) {
-            yes.push(candidates[j]);
-        } else {
-            no.push(candidates[j]);
-        }
-    }
-
-    console.log('yes');
-    console.log(yes.length);
-    console.log('no');
-    console.log(no.length);
-
-    return {
-        category: category,
-        yes: questionNode(yes),
-        no: questionNode(no),
-        candidates: candidates
-    };
-
-};
-fpObj(10, 10, 37.77493, -122.419415);
-
-
+questionTree(10, 10, 37.77493, -122.419415);
