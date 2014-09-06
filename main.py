4324ed14f0c8a4bd31d5dd020681da0d7452e776
Raghav Subramaniam <raghavs511@gmail.com>
2014-09-06 14:49:05 -0400

    Did port stuff

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 
 import argparse
@@ -76,4 +78,5 @@ def request():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
