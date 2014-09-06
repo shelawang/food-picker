e3ec44b99d1e5ba2ec9dd3b0539fd73ecf69c269
Raghav Subramaniam <raghavs511@gmail.com>
2014-09-06 01:03:17 -0400

    Fixed up interfaces for logic.js

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
     Given a set of search parameters, do a call to the Yelp API
     and return the resulting Food Picker JS object.
 */
-var fpObj = yelpAccess(limit, radius, latitude, longitude) {
+var fpObj = function(limit, radius, latitude, longitude) {
     return 0;
 };
 
@@ -10,6 +10,14 @@ var fpObj = yelpAccess(limit, radius, latitude, longitude) {
     Given a Food Picker JS object, generate a binary decision tree
     that contains the questions being used by Food Picker.
 */
-var questionTree = genQuestions(fpObj) {
-    return 0;
+var questionTree = function(fpObj) {
+
+};
+
+/*
+    Given a chunk of a Food Picker JS object, create one node of
+    the binary decision tree.
+*/
+var questionNode = function(fpObjRemaining) {
+
 };
