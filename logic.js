b9cdc18fc98e5cab347c5d4631471293baa301f0
rachely3n <ry88@cornell.edu>
2014-09-06 01:32:50 -0400

    donkey long

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
     Given a set of search parameters, do a call to the Yelp API
     and return the resulting Food Picker JS object.
 */
-var fpObj = function(limit, radius, latitude, longitude) {
+var fpObj = function(limit, radius, lat, long) {
     return 0;
 };
 
