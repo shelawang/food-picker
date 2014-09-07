d1711aff23c9bd7d506196231ce1f386b3acaf68
Raghav Subramaniam <raghavs511@gmail.com>
2014-09-07 10:28:13 -0400

    Ready for presentation

--- static/scripts/logic.js
+++ static/scripts/logic.js
@@ -40,7 +40,7 @@ var questionTree = function(limit, radius, lat, long_, cat) {
     var businesses;
     $.ajax({
         dataType: "json",
-        url: "http://food-picker-2.herokuapp.com/request",
+        url: "http://food-picker.herokuapp.com/request",
         data: data,
         success: function(result) {        
             businesses = result.businesses;
